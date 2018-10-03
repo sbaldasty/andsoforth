313945602c26afd8bfc987ebe60b344752c2776b
Steven Baldasty <steven.baldasty@opentempo.com>
2018-10-03 15:24:19 -0400

    More macro analysis tests.

--- src/test/kotlin/com/bitflippin/andsoforth/analysis/MacroAnalysisTest.kt
+++ src/test/kotlin/com/bitflippin/andsoforth/analysis/MacroAnalysisTest.kt
@@ -1,10 +1,9 @@
 package com.bitflippin.andsoforth.analysis
 
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
 import org.junit.Test
 
-class MacroTest {
+class MacroAnalysisTest {
 
     /**
      * An appropriate model is constructed for a macro that has no commands
@@ -34,5 +33,33 @@ class MacroTest {
         // And contain each other as commands
         assertEquals(listOf(meMacro), youMacro.commands)
         assertEquals(listOf(youMacro), meMacro.commands)
+    }
+
+    /**
+     * An appropriate model is constructed for a macro that contains multiple
+     * commands.
+     */
+    @Test
+    fun multipleCommands() {
+        val code = "/macro MultiMacro /begin X X /end /macro X /begin /end"
+        val environment = environment(code)
+        // The model exists
+        val multiMacro = environment.macros.getValue("MultiMacro")
+        val otherMacro = environment.macros.getValue("X")
+        // And has the two commands in it
+        assertEquals(listOf(otherMacro, otherMacro), multiMacro.commands)
+    }
+
+    /**
+     * An appropriate model is constructed for a macro that references itself.
+     */
+    @Test
+    fun recursive() {
+        val code = "/macro Macro /begin Macro /end"
+        val environment = environment(code)
+        // The model exists
+        val macro = environment.macros.getValue("Macro")
+        // And is its own command
+        assertEquals(listOf(macro), macro.commands)
     }
 }
